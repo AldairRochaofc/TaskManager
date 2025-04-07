8438878aabe8551045c9fe66977d3706bdade88c
Aldair Rocha <aldairmurilo000@gmail.com>
2025-04-07 17:39:15 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-function App () {
-  return(
-    <h1>hello world</h1>
-  )
+function App() {
+               return      <h1>hello world</h1>
 }
 export default App
--- src/assets/components/Tasks.jsx
+++ src/assets/components/Tasks.jsx
@@ -1,3 +1,1 @@
-function Tasks() {
-    
-}
+function Tasks() {}
